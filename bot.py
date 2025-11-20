308633fee728ca0891f59db2554c4f8bf8d1a597
durgeshraihero-commits <durgeshraihero@gmail.com>
2025-11-20 13:14:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ from telegram.ext import (
 
 # ================== CONFIG =====================
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8285505523:AAEoJgpBpVeUEErcseSSeCIEb0MwNAZ_5qM")
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
